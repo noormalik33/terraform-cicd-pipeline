faae8971d8c72df79956b91b06c4fa541e4c298f
Noor Malik <noormalik56300@gmail.com>
2025-12-13 14:29:32 +0500

    Switched to t3.micro for Free Tier eligibility

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "web_server" {
   source = "./modules/web_server"
 
   ami_id        = "ami-0030e4319cbf4dbf2" # Ubuntu 22.04 in us-east-1
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "terraform-key"         # Ensure this key exists in your AWS Console
   environment   = "prod"
 }
