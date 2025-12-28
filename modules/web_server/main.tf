6b6bebac155337fca7407c7743fcafad4aea1d11
Noor Malik <noormalik56300@gmail.com>
2025-12-28 22:00:36 +0500

    Fix: Switch back to t3.micro (Correct Free Tier)

--- modules/web_server/main.tf
+++ modules/web_server/main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "allow_ssh_http" {
 resource "aws_instance" "this" {
   # Task 6: Use the dynamic AMI ID found above (not hardcoded!)
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
